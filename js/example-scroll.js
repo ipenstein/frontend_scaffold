a0afa3c25c5f114fdbfde208b92cca7696562b13
Marc Wittenbrink <wittenbrink@gmail.com>
2015-06-20 10:06:59 +0200

    Remove location hash update due to performance problems

--- js/example-scroll.js
+++ js/example-scroll.js
@@ -12,7 +12,7 @@ function scrollSpy() {
             if ($(window).scrollTop() >= triggerPos[i]) {
                 $('.main-nav li').removeClass('active');
                 $('#nav-' + i.substr(1)).addClass('active');
-                self.document.location.hash = '#nav-' + i.substr(1);
+                //self.document.location.hash = '#nav-' + i.substr(1);
             }
         }
     });
