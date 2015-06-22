6b8b0b0d8e4b0076644be6d5ab0193fdcc6fe1a2
Marc Wittenbrink <wittenbrink@gmail.com>
2015-06-22 21:48:20 +0200

    Edit presenational styles

--- config.rb
+++ config.rb
@@ -11,7 +11,7 @@ javascripts_dir = "js"
 
 # Set up enviroment
 project_type = :stand_alone
-output_style = :nested
+output_style = :compressed
 line_comments = false
 relative_assets = true
 
--- js/tabs.js
+++ js/tabs.js
@@ -5,11 +5,11 @@ $.fn.initTabs = function() {
 
     // Wenn es den Inhalt nicht gibt - wird der Tab entfernt
     // au?rdem: href nach rel
-    $( ".tabs li" , u ).each ( function () {
-        var link = $( this ).find( "a" );
+    $( "> .tabs > li" , u ).each ( function () {
+        var link = $( "> a" , this);
         var haref = link.attr( 'href' );
 
-        if ( $( $( this ).find( "a" ).attr( "href" ) ).size() === 0 ) {
+        if ( $( $( "> a", this ).attr( "href" ) ).size() === 0 ) {
     //  if ( link.attr( "href" ) == 0 ) { --> Ist doch quatsch ... da werden nur Tabs ohne href entfernt und nicht Tabs ohne Ziel ...
             $( this ).remove () ;
         }
@@ -18,31 +18,31 @@ $.fn.initTabs = function() {
         link.attr( 'href' , null );
     } ) ;
 
-    $( ".tabs li" , u ).click ( function () {
-        $( ".tabs li" , u ).removeClass ( "active" ) ;
+    $( "> .tabs > li" , u ).click ( function () {
+        $( "> .tabs > li" , u ).removeClass ( "active" ) ;
         $( this ).addClass ( "active" ) ;
-        $( ".tab-content" , u ).hide () ;
+        $( "> .tab-content" , u ).hide () ;
 
-        var rel = $( this ).find( "a" ).attr( "rel" );
-        $( $( this ).find( "a" ).attr( "rel" ) ).fadeIn () ;
-        self.document.location.hash = 'panel-' + rel.replace('#', '');
+        var rel = $( "> a" , this ).attr( "rel" );
+        $( $( "> a" , this ).attr( "rel" ) ).fadeIn () ;
+    //    self.document.location.hash = 'panel-' + rel.replace('#', '');
     //          return false ;
     } ) ;
 
-    $( ".tab-content" , u ).hide () ;
+    $( "> .tab-content" , u ).hide () ;
 
-    if ( self.document.location.hash ) {
-        var h = self.document.location.hash.replace('panel-', '');
-        var el = $( 'a[rel=' + h + ']', u );
+    //if ( self.document.location.hash ) {
+    //    var h = self.document.location.hash.replace('panel-', '');
+    //    var el = $( 'a[rel=' + h + ']', u );
 
-        if (el.length) {
-            $( el ).parent().click () ;
-        } else {
-            $( ".tabs li:first" , u ).click () ;
-        }
-    } else {
-        $( ".tabs li:first" , u ).click () ;
-    }
+    //    if (el.length) {
+    //        $( el ).parent().click () ;
+    //    } else {
+    //        $( ".tabs li:first" , u ).click () ;
+    //    }
+    //} else {
+        $( "> .tabs > li:first" , u ).click () ;
+    //}
 }
 
 $( document ).ready ( function () {
