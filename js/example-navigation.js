59107828dc116a7d3879f185c319acc82869f898
Marc Wittenbrink <wittenbrink@gmail.com>
2015-06-28 00:33:01 +0200

    Revert "half-assed try for off-canvas navigation"
    This reverts commit 85480b861d7a22bf2c7f724d09b16f9679e24602.

--- js/example-navigation.js
+++ js/example-navigation.js
@@ -1,13 +1,5 @@
 // example-navigation
 
 $(document).ready(function() {
-    //$(".main-nav").initDropdown();
-
-    $(".open-panel").click(function(){
-        $("html").addClass("openNav");
-    });
-
-    $(".close-panel, .hero, .content").click(function(){
-        $("html").removeClass("openNav");
-    });
+    $(".main-nav").initDropdown();
 });
